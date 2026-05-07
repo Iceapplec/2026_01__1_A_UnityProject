a74074848e390830d97afe6b9c8517abe90c363d
Iceapplec <dhpinkanny0222@gmail.com>
2026-05-07 12:22:37 +0900

    124

--- Assets/Scripts/RankGame/DraggableRank.cs
+++ Assets/Scripts/RankGame/DraggableRank.cs
@@ -24,7 +24,7 @@ public class DraggableRank : MonoBehaviour
     {
         mainCamera = Camera.main;
         spriteRenderer = GetComponent<SpriteRenderer>();
-        gameManager = FindObjectOfType<RankGameManager>();
+        gameManager = FindAnyObjectByType<RankGameManager>();
     }
 
     void Start()
